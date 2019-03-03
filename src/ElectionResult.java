077206d589a07d94501dfc7b822ad3a90af801b6
justinHe123 <justinme100@gmail.com>
2019-03-02 20:44:47 -0800

    implemented toString, reformatted code

--- src/ElectionResult.java
+++ src/ElectionResult.java
@@ -1,114 +1,119 @@
-public class ElectionResult{
+public class ElectionResult {
     private double demVotes, gopVotes, totalVotes, percentDem, percentGop, pointDiff, pointDiffPercent;
     private String stateAbbr, countyName, combinedFips;
 
-    public ElectionResult(String line){
+    public ElectionResult(String line) {
         int index = line.indexOf("\"");
-        if(line.contains("\"")){
+        if (line.contains("\"")) {
             line = this.removeComma(line, index);
             line = line.replace("\"", "");
         }
-    String[] vals = line.split(",");
-    demVotes = Double.parseDouble(vals[1]);
-    gopVotes = Double.parseDouble(vals[2]);
-    totalVotes = Double.parseDouble(vals[3]);
-    percentDem = Double.parseDouble(vals[4]);
-    percentGop = Double.parseDouble(vals[5]);
-    pointDiff = Double.parseDouble(vals[6]);
-    pointDiffPercent = Double.parseDouble(vals[7].substring(0, vals[7].length() - 1)) / 100;
-    stateAbbr = vals[8];
-    countyName = vals[9];
-    combinedFips = vals[10];
-}
+        String[] vals = line.split(",");
+        demVotes = Double.parseDouble(vals[1]);
+        gopVotes = Double.parseDouble(vals[2]);
+        totalVotes = Double.parseDouble(vals[3]);
+        percentDem = Double.parseDouble(vals[4]);
+        percentGop = Double.parseDouble(vals[5]);
+        pointDiff = Double.parseDouble(vals[6]);
+        pointDiffPercent = Double.parseDouble(vals[7].substring(0, vals[7].length() - 1)) / 100;
+        stateAbbr = vals[8];
+        countyName = vals[9];
+        combinedFips = vals[10];
+    }
 
-    private String removeComma(String line, int startIndex){
+    private String removeComma(String line, int startIndex) {
         int maxIndex = line.indexOf("\"", startIndex + 1);
         int index = line.indexOf(",", startIndex);
-        if(index < maxIndex){
+        if (index < maxIndex) {
             line = line.substring(0, index) + line.substring(index + 1, line.length());
             line = removeComma(line, index);
         }
         return line;
     }
 
-    public double getDemVotes(){
+    public double getDemVotes() {
         return demVotes;
     }
 
-    public void setDemVotes(double votes){
+    public void setDemVotes(double votes) {
         demVotes = votes;
     }
 
-    public double getGopVotes(){
+    public double getGopVotes() {
         return gopVotes;
     }
 
-    public void setGopVotes(double votes){
+    public void setGopVotes(double votes) {
         gopVotes = votes;
     }
 
-    public double getTotalVotes(){
+    public double getTotalVotes() {
         return totalVotes;
     }
 
-    public void setTotalVotes(double votes){
+    public void setTotalVotes(double votes) {
         totalVotes = votes;
     }
 
-    public double getPercentDem(){
+    public double getPercentDem() {
         return percentDem;
     }
 
-    public void setPercentDem(double percent){
+    public void setPercentDem(double percent) {
         percentDem = percent;
     }
 
-    public double getPercentGop(){
+    public double getPercentGop() {
         return percentGop;
     }
 
-    public void setPercentGop(double percent){
+    public void setPercentGop(double percent) {
         percentGop = percent;
     }
 
-    public double getPointDiff(){
+    public double getPointDiff() {
         return pointDiff;
     }
 
-    public void setPointDiff(double diff){
+    public void setPointDiff(double diff) {
         pointDiff = diff;
     }
 
-    public Double getPointDiffPercent(){
+    public Double getPointDiffPercent() {
         return pointDiffPercent;
     }
 
-    public void setPointDiffPercent(double percent){
+    public void setPointDiffPercent(double percent) {
         pointDiffPercent = percent;
     }
 
-    public String getStateAbbr(){
+    public String getStateAbbr() {
         return stateAbbr;
     }
 
-    public void setStateAbbr(String abbr){
+    public void setStateAbbr(String abbr) {
         stateAbbr = abbr;
     }
 
-    public String getCountyName(){
+    public String getCountyName() {
         return countyName;
     }
 
-    public void setCountyName(String name){
+    public void setCountyName(String name) {
         countyName = name;
     }
 
-    public String getCombinedFips(){
+    public String getCombinedFips() {
         return combinedFips;
     }
 
-    public void setCombinedFips(String fips){
+    public void setCombinedFips(String fips) {
         combinedFips = fips;
+    }
+
+    public String toString() {
+        return demVotes + ", " + gopVotes + ", " + totalVotes + ", " + percentDem + ", " + percentGop + ", "
+                + pointDiff + ", " + pointDiffPercent + ", " + stateAbbr + ", " + countyName + ", " + combinedFips;
     }
 
 }
