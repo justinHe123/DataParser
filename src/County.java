e2a6d80a1b3ed05c0d0d68639f282c6d486802eb
justinHe123 <justinme100@gmail.com>
2019-03-09 21:05:03 -0800

    added extra utility methods

--- src/County.java
+++ src/County.java
@@ -1,17 +1,15 @@
 public class County {
     private String name;
     private int fips;
-    private Election2016 vote2016;
+    private Election2016 elec2016;
     private Education2016 educ2016;
     private Employment2016 employ2016;
 
-    public County(String name, int fips, Election2016 vote2016, Education2016 educ2016, Employment2016 employ2016) {
+    public County(String name, int fips) {
         this.name = name;
         this.fips = fips;
-        this.vote2016 = vote2016;
-        this.educ2016 = educ2016;
-        this.employ2016 = employ2016;
     }
+
 
     public String getName() {
         return name;
@@ -30,11 +28,11 @@ public class County {
     }
 
     public Election2016 getVote2016() {
-        return vote2016;
+        return elec2016;
     }
 
     public void setVote2016(Election2016 vote2016) {
-        this.vote2016 = vote2016;
+        this.elec2016 = vote2016;
     }
 
     public Education2016 getEduc2016() {
