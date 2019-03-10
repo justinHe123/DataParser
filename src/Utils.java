c08e627fbbe8cc1e3fb19d6c0eb4348af9dc8d16
justinHe123 <justinme100@gmail.com>
2019-03-10 15:33:58 -0700

    shortened parseAll using helpers

--- src/Utils.java
+++ src/Utils.java
@@ -20,37 +20,21 @@ public class Utils {
 
     public static DataManager parseAllData() {
         DataManager data = new DataManager();
-        List<State> states = data.getStates();
+        parseElectionResults(data);
+        parseEducationData(data);
+        parseEmploymentData(data);
+        return data;
+    }
+
+    private static void parseElectionResults(DataManager data) {
         String[] electionData = readFileAsString("data/2016_Presidential_Results.csv").split("\n");
         for (int i = 1; i < electionData.length; i++) {
             String[] vals = formatData(electionData[i]).split(",");
             addElection2016(data, vals);
         }
-        String[] educationData = readFileAsString("data/Education.csv").split("\n");
-        for (int i = 5; i < educationData.length; i++) {
-            String[] vals = formatData(educationData[i]).split(",");
-            addEducation2016(data, vals);
-        }
-        String[] employmentData = readFileAsString("data/Unemployment.csv").split("\n");
-        for (int i = 8; i < employmentData.length; i++) {
-            String[] vals = formatData(employmentData[i]).split(",");
-            addEmployment2016(data, vals);
-        }
-
-        return data;
     }
 
-    public static ArrayList<Election2016> parseElectionResults(String file) {
-        String[] data = file.split("\n");
-        ArrayList<Election2016> results = new ArrayList<>();
-        for (int i = 1; i < data.length; i++) {
-            String[] vals = data[i].split(",");
-            results.add(createElection2016(vals));
-        }
-        return results;
-    }
-
-    public static void addElection2016(DataManager data, String[] vals) {
+    private static void addElection2016(DataManager data, String[] vals) {
         try {
             Election2016 election = createElection2016(vals);
             County county = findCounty(data, vals, 8, 10, 9);
@@ -60,7 +44,7 @@ public class Utils {
         }
     }
 
-    public static Election2016 createElection2016(String[] vals) {
+    private static Election2016 createElection2016(String[] vals) {
         double demVotes = Double.parseDouble(vals[1]);
         double gopVotes = Double.parseDouble(vals[2]);
         double totalVotes = Double.parseDouble(vals[3]);
@@ -68,17 +52,15 @@ public class Utils {
 
     }
 
-    public static ArrayList<Education2016> parseEducationData(String file) {
-        String[] data = file.split("\n");
-        ArrayList<Education2016> results = new ArrayList<>();
-        for (int i = 5; i < data.length; i++) {
-            String[] vals = formatData(data[i]).split(",");
-            results.add(createEducation2016(vals));
+    private static void parseEducationData(DataManager data) {
+        String[] educationData = readFileAsString("data/Education.csv").split("\n");
+        for (int i = 5; i < educationData.length; i++) {
+            String[] vals = formatData(educationData[i]).split(",");
+            addEducation2016(data, vals);
         }
-        return results;
     }
 
-    public static void addEducation2016(DataManager data, String[] vals) {
+    private static void addEducation2016(DataManager data, String[] vals) {
         try {
             Education2016 education = createEducation2016(vals);
             County county = findCounty(data, vals, 1, 0, 2);
@@ -88,7 +70,7 @@ public class Utils {
         }
     }
 
-    public static Education2016 createEducation2016(String[] vals) {
+    private static Education2016 createEducation2016(String[] vals) {
         double noHighSchool = Double.parseDouble(vals[43]);
         double onlyHighSchool = Double.parseDouble(vals[44]);
         double someCollege = Double.parseDouble(vals[45]);
@@ -96,17 +78,15 @@ public class Utils {
         return new Education2016(noHighSchool, onlyHighSchool, someCollege, bachelorsOrMore);
     }
 
-    public static ArrayList<Employment2016> parseEmploymentData(String file) {
-        String[] data = file.split("\n");
-        ArrayList<Employment2016> results = new ArrayList<>();
-        for (int i = 8; i < data.length; i++) {
-            String[] vals = formatData(data[i]).split(",");
-            results.add(createEmployment2016(vals));
+    private static void parseEmploymentData(DataManager data) {
+        String[] employmentData = readFileAsString("data/Unemployment.csv").split("\n");
+        for (int i = 8; i < employmentData.length; i++) {
+            String[] vals = formatData(employmentData[i]).split(",");
+            addEmployment2016(data, vals);
         }
-        return results;
     }
 
-    public static void addEmployment2016(DataManager data, String[] vals) {
+    private static void addEmployment2016(DataManager data, String[] vals) {
         try {
             Employment2016 employment = createEmployment2016(vals);
             County county = findCounty(data, vals, 1, 0, 2);
@@ -116,7 +96,7 @@ public class Utils {
         }
     }
 
-    public static Employment2016 createEmployment2016(String[] vals) {
+    private static Employment2016 createEmployment2016(String[] vals) {
         int totalLaborForce = Integer.parseInt(vals[42].trim());
         int employedLaborForce = Integer.parseInt(vals[43].trim());
         int unemployedLaborForce = Integer.parseInt(vals[44].trim());
