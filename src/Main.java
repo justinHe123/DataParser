fdd0223ec8d72cb2a7bca46c9426ba4b04510e96
justinHe123 <justinme100@gmail.com>
2019-03-16 19:47:53 -0700

    implemented methods to split the data by national average, and to write the data to files

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
-import javax.xml.crypto.Data;
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    final static double NO_HS_AVG = 13.0;
+
     public static void main(String[] args) {
 //        DataManager data = Utils.parseAllData();
         DataManager data = findCompleteDatasets(Utils.parseAllData());
-        for (State state : data.getStates()){
-            for (County county : state.getCounties())
-                System.out.println(state.getName() + "," + county.toString());
-        }
+//        for (State state : data.getStates()){
+//            for (County county : state.getCounties())
+//                System.out.println(state.getName() + "," + county.toString());
+//        }
+        List<DataManager> splitData = splitByNatAvg(data);
+//        System.out.println("ABOVE");
+//        for (State state : splitData.get(0).getStates()){
+//            for (County county : state.getCounties())
+//                System.out.println(state.getName() + "," + county.toString());
+//        }
+//        System.out.println("BELOW");
+//        for (State state : splitData.get(1).getStates()){
+//            for (County county : state.getCounties())
+//                System.out.println(state.getName() + "," + county.toString());
+//        }
+        saveDataToFile("aboveNationalAverage.csv", splitData.get(0));
+        saveDataToFile("belowNationalAverage.csv", splitData.get(1));
     }
 
     public static DataManager findCompleteDatasets(DataManager data){
@@ -28,4 +43,45 @@ public class Main {
         }
         return completeData;
     }
+
+    public static List<DataManager> splitByNatAvg(DataManager data){
+        List<DataManager> splitData = new ArrayList<DataManager>();
+        DataManager above = new DataManager();
+        DataManager below = new DataManager();
+        for (State state : data.getStates()) {
+            State state1 = new State(state.getName());
+            State state2 = new State(state.getName());
+            above.add(state1);
+            below.add(state2);
+            for (County county : state.getCounties()) {
+                if (county.getEduc2016().getNoHighSchool() > NO_HS_AVG) state1.add(county);
+                else state2.add(county);
+            }
+        }
+        splitData.add(above);
+        splitData.add(below);
+        return splitData;
+    }
+
+    public static void saveDataToFile(String file, DataManager dm){
+        String data = "";
+        for (State state : dm.getStates()){
+            for (County county : state.getCounties()){
+                data += state.getName() + "," + county.toString() + "\n";
+            }
+        }
+        writeDataToFile(file, data);
+    }
+
+
+    private static void writeDataToFile(String filePath, String data){
+        File outFile = new File(filePath);
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(outFile))) {
+            writer.write(data);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
